ed8c244b75d66b1c3bf59f7b8358465d2c16feb7
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2024-08-10 00:18:56 -0800

    ...

---
+++ 01_compressed_file/compressed_file/target/debug/compressed_file.d
@@ -0,0 +1,1 @@
+E:\rust\ development\50-rust-projects\01_compressed_file\compressed_file\target\debug\compressed_file.exe: E:\rust\ development\50-rust-projects\01_compressed_file\compressed_file\src\main.rs
---
+++ 01_compressed_file/compressed_file/target/debug/deps/compressed_file.d
@@ -0,0 +1,5 @@
+E:\rust development\50-rust-projects\01_compressed_file\compressed_file\target\debug\deps\compressed_file.exe: src\main.rs
+
+E:\rust development\50-rust-projects\01_compressed_file\compressed_file\target\debug\deps\compressed_file.d: src\main.rs
+
+src\main.rs:
